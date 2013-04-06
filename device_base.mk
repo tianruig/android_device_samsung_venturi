9e8f2da10748b10431a50069cd18557d5acbb31f
JackpotClavin <jonclavin@gmail.com>
2013-04-06 18:10:55 -0400

    This external switch screws up the mount points now that our switchable vold is correct (/storage/sdcard*) so remove it.

--- device_base.mk
+++ device_base.mk
@@ -176,8 +176,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Set default USB interface and default to internal SD as /sdcard
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    persist.sys.usb.config=mass_storage \
-    persist.sys.vold.switchexternal=1
+    persist.sys.usb.config=mass_storage
 
 include frameworks/native/build/phone-hdpi-512-dalvik-heap.mk
 
