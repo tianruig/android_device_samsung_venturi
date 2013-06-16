e56831322f819c8eb010c511f0f1a7b9ac122d84
Samuel Holland <mevordel@gmail.com>
2013-06-16 15:30:15 -0500

    Use more versatile extract-files.sh
    Change-Id: Ieb9b51707a3a4621d501c9fe8d9f526e94302dc1

--- extract-files.sh
+++ extract-files.sh
@@ -2,16 +2,39 @@
 
 VENDOR=samsung
 DEVICE=venturi
-
 BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
 
-echo "Pulling common files..."
+rm -rf ../../../vendor/$VENDOR/$DEVICE/*
+mkdir -p $BASE
+
+if [ -f "$1" ]; then
+	rm -rf tmp
+	mkdir tmp
+	unzip -q "$1" -d tmp
+	if [ $? != 0 ]; then
+		echo "$1 is not a valid zip file. Bye."
+		exit 1
+	fi
+	echo "$1 successfully unzipped. Copying files..."
+	ZIP="true"
+else
+	echo "Pulling files..."
+	unset ZIP
+fi
+
+
 for FILE in `cat proprietary-files.txt | grep -v ^# | grep -v ^$`; do
-    DIR=`dirname $FILE`
-    if [ ! -d $BASE/$DIR ]; then
-        mkdir -p $BASE/$DIR
-    fi
-    adb pull /system/$FILE $BASE/$FILE
+	DIR=`dirname $FILE`
+	if [ ! -d $BASE/$DIR ]; then
+		mkdir -p $BASE/$DIR
+	fi
+	if [ "$ZIP" ]; then
+		cp tmp/system/$FILE $BASE/$FILE
+	else
+		adb pull /system/$FILE $BASE/$FILE
+	fi
 done
+
+if [ "$ZIP" ]; then rm -rf tmp ; fi
 
 ./setup-makefiles.sh
