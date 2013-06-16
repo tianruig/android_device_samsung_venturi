cd1fa3a24ab5501d52c27d80b28ea2f9ab6cd403
Samuel Holland <mevordel@gmail.com>
2013-06-16 16:02:00 -0500

    We have gotten USB storage to work, so enable it. * Also, call internal storage what it is.
    Change-Id: I103e1e7c580feef5c9fec6c013b9b139a917c3a9

--- device_base.mk
+++ device_base.mk
@@ -114,7 +114,8 @@ PRODUCT_PACKAGES += \
 
 # Device-specific packages
 PRODUCT_PACKAGES += \
-	SamsungServiceMode
+	SamsungServiceMode \
+	com.android.future.usb.accessory
 
 # Charger
 PRODUCT_PACKAGES += \
@@ -135,7 +136,7 @@ PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
 	frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
 	frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
-	packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml
+	frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml
 
 # The OpenGL ES API level that is natively supported by this device.
 # This is a 16.16 fixed point number
