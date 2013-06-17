005229c3795c910ac1e4e59085a3263a4fb70214
Samuel Holland <mevordel@gmail.com>
2013-06-16 19:12:51 -0500

    Modify for kernel with integrated ramdisks * Fix makefile * Remove python script
    Change-Id: I31e60e27448a6aacda467e45330fdece6e184578

--- shbootimg.mk
+++ shbootimg.mk
@@ -1,9 +1,16 @@
 LOCAL_PATH := $(call my-dir)
 
-INSTALLED_BOOTIMAGE_TARGET := $(PRODUCT_OUT)/boot.img
-$(INSTALLED_BOOTIMAGE_TARGET): $(INSTALLED_KERNEL_TARGET) $(recovery_ramdisk) $(INSTALLED_RAMDISK_TARGET) $(PRODUCT_OUT)/utilities/flash_image $(PRODUCT_OUT)/utilities/busybox $(PRODUCT_OUT)/utilities/make_ext4fs
+# Gzip within gzip is unnecessary and slow
+uncompressed_ramdisk := $(PRODUCT_OUT)/ramdisk.cpio
+$(uncompressed_ramdisk): $(INSTALLED_RAMDISK_TARGET)
+	zcat $< > $@
+
+# Ramdisk and recovery are required to build kernel
+TARGET_KERNEL_BINARIES : $(recovery_ramdisk) $(uncompressed_ramdisk) $(PRODUCT_OUT)/utilities/busybox $(PRODUCT_OUT)/utilities/make_ext4fs
+
+$(INSTALLED_BOOTIMAGE_TARGET): $(INSTALLED_KERNEL_TARGET) | $(ACP)
 	$(call pretty,"Boot image: $@")
-	$(hide) ./device/samsung/venturi/mkshbootimg.py $@ $(INSTALLED_KERNEL_TARGET) $(INSTALLED_RAMDISK_TARGET) $(recovery_ramdisk)
+	$(hide) $(ACP) $(INSTALLED_KERNEL_TARGET) $@
 
 $(INSTALLED_RECOVERYIMAGE_TARGET): $(INSTALLED_BOOTIMAGE_TARGET)
-	$(ACP) $(INSTALLED_BOOTIMAGE_TARGET) $@
+	$(hide) $(ACP) $(INSTALLED_BOOTIMAGE_TARGET) $@
