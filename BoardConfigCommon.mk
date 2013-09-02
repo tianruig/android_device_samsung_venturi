6705aafce4fef3503b9047e1c236add309bf7fad
Sultan (android1234567) <sultanxda@gmail.com>
2013-09-01 22:42:54 -0700

    BoardConfigCommon: remove flag 'TARGET_ARCH_VARIANT_CPU'
    This flag doesn't do anything for us.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -26,7 +26,6 @@ TARGET_ARCH := arm
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_ARCH_VARIANT_CPU := cortex-a8
 TARGET_CPU_VARIANT := cortex-a8
 ARCH_ARM_HAVE_VFP := true
 ARCH_ARM_HAVE_NEON := true
