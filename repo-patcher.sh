87f386d0bd0f8e206d83071633c76df5028e6b29
Sultanxda <sultanxda@gmail.com>
2014-02-09 12:43:50 -0800

    repo-patcher: Fix path derps

--- repo-patcher.sh
+++ repo-patcher.sh
@@ -4,8 +4,8 @@ echo Patching each individual repo for regular building...
 
 cd build
 git reset --hard && git clean -f -d
-patch -p1 < $HOME/cm11.0/device/htc/pyramid/patches/build.patch
+patch -p1 < ../device/samsung/venturi/patches/build.patch
 cd ../frameworks/opt/telephony
 git reset --hard && git clean -f -d
-patch -p1 < ../../../device/htc/pyramid/patches/frameworks-opt-telephony.patch
+patch -p1 < ../../../device/samsung/venturi/patches/frameworks-opt-telephony.patch
 cd ../../..
