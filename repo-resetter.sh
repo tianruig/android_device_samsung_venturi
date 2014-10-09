403db6fab7b964c39fb080e4fb9f1cc79227a4b6
Tianrui Guo <tianruig@gmail.com>
2014-10-09 14:53:05 -0700

    Remove these files

--- repo-resetter.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Reset each repo in order to successfully repo sync
-echo Resetting each repo...
-cd build
-git reset --hard && git clean -f -d
-cd ../frameworks/opt/telephony
-git reset --hard && git clean -f -d
-cd ../../../
